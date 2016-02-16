cfb1a2131c72dc24d209025dbc084aa04f508066
Chris Donofrio <cdonofrio20@gmail.com>
2016-02-16 18:06:20 -0500

    finished orm.js

--- config/orm.js
+++ config/orm.js
@@ -5,7 +5,26 @@ Here is the O.R.M. where you write functions that takes inputs and conditions an
 var connection = require('../config/connection.js');
 
 var orm = {
-
+    chooseBurger: function(burger, cb) {
+        var sql = "INSERT INTO burgers (burger_name) VALUES ('" + burger + "')";
+        connection.query(sql, function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    devourBurger: function(burgerId, cb) {    
+    	var id = burgerId;
+		connection.query("UPDATE burgers SET devoured = 1 WHERE id = ?", [id], function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+  	showBurgers: function(tableName, cb) {
+    	connection.query('SELECT * FROM burgers', function(err, result) {
+      		if (err) throw err;
+      		cb(result);
+  		});
+ 	}
 };
     
 module.exports = orm;
