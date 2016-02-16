fb7faf607a963688ddcf5811fbbdb31f8ab01899
Chris Donofrio <cdonofrio20@gmail.com>
2016-02-16 18:15:59 -0500

    setup functions for database interaction

--- models/burger.js
+++ models/burger.js
@@ -4,6 +4,25 @@ Setup the functions for how app interacts with the database.
 
 var orm = require('../config/orm.js');
 
-var burgers =
+var burgers = {
 
-module.exports = 
+  add: function (burger, cb) {
+    orm.chooseBurger(burger, function (res){
+      cb(res);
+    });
+  },
+
+  eat: function (burgerId, cb) {
+    orm.devourBurger(burgerId, function (res){
+      cb(res);
+    })
+  },
+
+  show: function (cb) {
+    orm.showBurgers('tableName', function (res) {
+      cb(res);
+    });
+  }
+};
+
+module.exports = burgers;
