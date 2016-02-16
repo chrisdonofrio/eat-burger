5b6dee07c2b3c6e970c485013043d0dcdff28cae
Chris Donofrio <cdonofrio20@gmail.com>
2016-02-15 19:27:18 -0500

    created database schema and inserted values

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("bacon cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("veggie", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("portobello", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("hickory bbq", false);
